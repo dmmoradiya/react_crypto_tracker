b789986fcbd7e656b60037916aa534d88d62247b
dmmoradiya <dhruv.moradiya365@gmail.com>
2023-05-25 14:17:15 +0530

    setup route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { makeStyles } from "@material-ui/core";
 import Homepage from "./Pages/HomePage";
 import "./App.css";
-import { Buffer } from 'buffer';
 import { BrowserRouter, Route } from "react-router-dom";
 import CoinPage from "./Pages/CoinPage";
 import Header from "./components/Header";
+import { HashRouter, Router } from "react-router-dom/cjs/react-router-dom.min";
 
 const useStyles = makeStyles(() => ({
   App: {
@@ -18,13 +18,13 @@ function App() {
   const classes = useStyles();
 
   return (
-    <BrowserRouter>
-      <div className={classes.App}>
-        <Header />
-        <Route path="/" component={Homepage} exact />
-        <Route path="/coins/:id" component={CoinPage} exact />
-      </div>
-    </BrowserRouter>
+      <HashRouter basename="/">
+        <div className={classes.App}>
+          <Header />
+          <Route path="/" component={Homepage} exact />
+          <Route path="/coins/:id" component={CoinPage} exact />
+        </div>
+      </HashRouter>
   );
 }
 
